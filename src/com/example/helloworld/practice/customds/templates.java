f71a6e13730bca2fe793a04350a873be7c3eddf8
ameerpetbatch <cmrs5982@gmail.com>
2017-07-31 11:58:29 -0700

    Update templates.java

--- src/com/example/helloworld/practice/customds/templates.java
+++ src/com/example/helloworld/practice/customds/templates.java
@@ -24,12 +24,25 @@ class Graph<T> {
     public List<Integer> getNeighbors(int v) {return adj.get(v); }
 }
 
-class TrieNode{
-  TrieNode[] list = new TrieNode[26];
-  String word; //populated only at the end of trienode
-  boolean isEnd; //populated only at the end of the trienode, either word or isEnd can be at each trienode
+class Trie{
+    TrieNode root;
+    class TrieNode {
+        // R links to node children
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+        private String endWord;
+        public TrieNode() {links = new TrieNode[R];}
+        public boolean containsKey(char ch) {return links[ch -'a'] != null;}
+        public TrieNode get(char ch) {return links[ch -'a'];}
+        public void put(char ch, TrieNode node) {links[ch -'a'] = node;}
+        public void setEnd() {isEnd = true;}
+        public boolean isEnd() {return isEnd;}
+    }
+    void buildTrie(String word);
+    boolean search(String word);
+    boolean startsWith(String prefixWord);
 }
-
  public static class NestedInteger {
         private Integer value;
         private List<NestedInteger> list;
@@ -56,11 +69,23 @@ class TrieNode{
 }
 
 Stack
+    Stack<K> stack = new Stack<K>();
+    stack.push(); stack.pop(); stack.size(); stack.isEmpty();
 
 Queue
+    Queue<K> queue = new LinkedList<K>();
+    queue.add(K val); queue.remove(); queue.size(); queue.isEmpty();
 
 PriorityQueue
+    PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>(){
+             @override
+            public int compare(Node n1, Node n2){ return Integer.compare(n1.value, n2.value));}
+    });
 
 Heap
+    Min-heap eg: PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+    Max-heap eg: PriorityQueue<Integer> pq2 = new PriorityQueue<Integer>(Collections.reverseOrder());
 
 Deque
+    Deque deque = new LinkedList<>(); deque.offerFirst(); deque.offerLast(); //adding it in tail
+    deque.removeFirst(); deque.removeLast();
